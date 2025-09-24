0723a33b40768d70de2b23ecc59990868e635234
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-09-25 00:49:33 +0300

    fix2 mistace conflicts

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-<<<<<<< HEAD
-      {
-        protocol: 'https',
-        hostname: 'ftp.goit.study',
-        pathname: '/img/lehlehka/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        pathname: '/**',
-      },
-=======
+
       { protocol: 'https', hostname: 'ac.goit.global', pathname: '/**' },
       { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
->>>>>>> main
     ],
   },
 
