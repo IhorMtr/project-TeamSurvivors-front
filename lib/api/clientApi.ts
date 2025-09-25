76ce640950da60463b9736e6048508aba5a1df90
IhorMtr <igormotornyyy@gmail.com>
2025-09-25 19:22:50 +0300

    Refactor API response handling in clientApi to return nested data structure

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,11 +1,17 @@
+import { NewDiary } from '@/types/diaryEntry';
 import { api } from './auth';
 
 export const getDiaries = async () => {
   const response = await api.get('/diaries');
-  return response.data;
+  return response.data.data;
 };
 
 export const getDiariesById = async (id: string) => {
   const response = await api.get(`/diaries/${id}`);
-  return response.data;
+  return response.data.data;
+};
+
+export const createDiary = async (data: NewDiary) => {
+  const response = await api.post('/api/diaries', data);
+  return response.data.data;
 };
