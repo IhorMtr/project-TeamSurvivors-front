d7983f0fdd837f2d100ff397b4287bd047880dde
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-09-27 17:59:29 +0300

    feat: continue journey page improvements

--- components/WeekSelector/WeekSelector.tsx
+++ components/WeekSelector/WeekSelector.tsx
@@ -2,25 +2,31 @@
 
 import WeekButton from "../WeekButton/WeekButton";
 import styles from './WeekSelector.module.css';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useRouter } from "next/navigation";
-
+import PuffLoader from "react-spinners/PuffLoader";
 
 interface WeekSelectorProps {
   currentWeek: number;
 }
 
-const WeekSelector: React.FC<WeekSelectorProps> = ({currentWeek}) => {
+const WeekSelector: React.FC<WeekSelectorProps> = ({ currentWeek }) => {
   const router = useRouter();
   const allWeeks = Array.from({ length: 40 }, (_, i) => i + 1);
   const containerRef = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedWeek, setSelectedWeek] = useState(currentWeek);
 
-  const handleWeekClick = (weekNumber: number) => {
-    if (weekNumber <= currentWeek) {
+  const handleWeekClick = async (weekNumber: number) => {
+    if (weekNumber <= currentWeek && weekNumber !== selectedWeek) {
+      setIsLoading(true);
+      setSelectedWeek(weekNumber);
+      
+      await new Promise(resolve => setTimeout(resolve, 300));
+      
       router.push(`/journey/${weekNumber}`);
     }
   }
-
 
   const scrollToCurrentWeek = () => {
     if (containerRef.current) {
@@ -43,18 +49,33 @@ const WeekSelector: React.FC<WeekSelectorProps> = ({currentWeek}) => {
     scrollToCurrentWeek();
   }, [currentWeek]);
 
-  return (
-    <div ref={containerRef} className={styles.weekSelector}>
-      {allWeeks.map((week) => (
-        <WeekButton 
-        key={week} 
-        weekNumber={week}
-        isCurrent={week === currentWeek}
-        isDisabled={week > currentWeek}
-        onClick={() => handleWeekClick(week)}
-      />
+  useEffect(() => {
+    if (selectedWeek === currentWeek) {
+      setIsLoading(false);
+    }
+  }, [currentWeek, selectedWeek]);
 
-      ))}
+  return (
+    <div className={styles.weekSelectorContainer}>
+      <div ref={containerRef} className={styles.weekSelector}>
+        {allWeeks.map((week) => (
+          <WeekButton 
+            key={week} 
+            weekNumber={week}
+            isCurrent={week === currentWeek}
+            isDisabled={week > currentWeek || isLoading}
+            onClick={() => handleWeekClick(week)}
+          />
+        ))}
+      </div>
+      
+      Лоадер под неделями
+      {isLoading && (
+        <div className={styles.loaderContainer}>
+          <PuffLoader size={40} color="#36d7b7" />
+          <span>Завантаження даних...</span>
+        </div>
+      )}
     </div>
   );
 };
