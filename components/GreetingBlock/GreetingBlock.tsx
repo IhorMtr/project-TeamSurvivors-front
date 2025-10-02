98be8870c6850807f6acd06be09d4bff476c6386
IhorMtr <igormotornyyy@gmail.com>
2025-10-02 18:15:38 +0300

    feat: add dynamic greeting based on time of day and fallback for unauthorized users

--- components/GreetingBlock/GreetingBlock.tsx
+++ components/GreetingBlock/GreetingBlock.tsx
@@ -3,10 +3,27 @@ import css from './GreetingBlock.module.css';
 
 const GreetingBlock = () => {
   const user = useAuthStore(state => state.user);
-  const title = user.name;
+  const title = user?.name || 'користувач';
+
+  const getGreeting = () => {
+    const hours = new Date().getHours();
+
+    if (hours >= 5 && hours < 12) {
+      return 'Доброго ранку';
+    } else if (hours >= 12 && hours < 18) {
+      return 'Доброго дня';
+    } else if (hours >= 18 && hours < 23) {
+      return 'Доброго вечора';
+    } else {
+      return 'Доброї ночі';
+    }
+  };
+
   return (
     <div className={css.container}>
-      <h1 className={css.titleDashboard}>Доброго ранку, {title}!</h1>
+      <h1 className={css.titleDashboard}>
+        {getGreeting()}, {title}!
+      </h1>
     </div>
   );
 };
